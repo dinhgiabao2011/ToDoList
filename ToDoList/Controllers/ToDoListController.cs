8e8d884989742d8238cf3dc464d00ef77155f88b
dinhgiabao2011 <dinhbb999@gmail.com>
2021-12-01 15:06:38 +0700

    Add project files.

---
+++ ToDoList/Controllers/ToDoListController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Data;
+
+namespace ToDoList.Controllers
+{
+	public class ToDoListController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ToDoListController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var obj = _context.ToDoes.ToList();
+			return View(obj);
+		}
+
+
+	}
+}
---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+	public class ToDo
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Context { get; set; }
+	}
+}
